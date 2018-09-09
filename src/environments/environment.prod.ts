a8a4920460544c06240b821b76cb9635c0fa602b
Santanu Kumar Sahu <sanmeranam@gmail.com>
2018-09-10 00:19:39 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA93nr8fykJcqCRaWik9c6-o5Wpp0IYKgg",
-    authDomain: "mdesigner-0.firebaseapp.com",
-    databaseURL: "https://mdesigner-0.firebaseio.com",
-    projectId: "mdesigner-0",
-    storageBucket: "mdesigner-0.appspot.com",
-    messagingSenderId: "628622091266"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
